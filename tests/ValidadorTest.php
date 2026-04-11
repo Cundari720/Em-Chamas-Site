8db924a629d568b4de9ec6b93b16f38f5613bbd5
Matheus Lopes Cundari <matheus.cundari23@gmail.com>
2026-04-11 19:29:40 -0300

    Add tests for Validador class methods

--- tests/ValidadorTest.php
+++ tests/ValidadorTest.php
@@ -1,1 +1,17 @@
+<?php
 
+use PHPUnit\Framework\TestCase;
+use App\Validador;
+
+class ValidadorTest extends TestCase
+{
+    public function testValidarPedidoComDadosCorretos()
+    {
+        $this->assertTrue(Validador::validarPedido("Matheus", "Paz e saúde"));
+    }
+
+    public function testValidarPedidoComDadosVazios()
+    {
+        $this->assertFalse(Validador::validarPedido("", "   "));
+    }
+}
