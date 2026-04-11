9387efcc985506182f514eb88a3f6a5e9361d040
Matheus Lopes Cundari <matheus.cundari23@gmail.com>
2026-04-11 19:31:31 -0300

    Update validador.php

--- src/validador.php
+++ src/validador.php
@@ -1,1 +1,13 @@
+<?php
+namespace App;
 
+class Validador {
+    public static function validarPedido($nome, $pedido) {
+        // Retorna falso se o nome ou o pedido estiverem em branco
+        if (empty(trim($nome)) || empty(trim($pedido))) {
+            return false;
+        }
+        return true;
+    }
+}
+?>
